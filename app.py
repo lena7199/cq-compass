ab2c2286b58ff9119b0e30e1622b60be87d40b18
lena7199 <pari.elen@gmail.com>
2026-08-16 21:57:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1562,7 +1562,7 @@ def main():
         /* 1. DEFINE THE PALETTE (Change these 5 hex codes to change the whole app!) */
         :root {
             /* 1. MAIN UI COLORS (Change these to test new palettes!) */
-            --bg-main: #F7F7F7;          /* General Background */
+            --bg-main: #8B8589;          /* General Background */
             --bg-secondary: #635656;     /* Cards, Inputs, Dropdowns */
             --text-main: #111827;        /* Primary Text */
             --text-secondary: #A8A492;   /* Borders, Subtle Text, Icons */
